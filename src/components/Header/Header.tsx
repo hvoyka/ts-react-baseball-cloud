05518c38fbaec06b9d56992a69afbd1be9e89183
Lavrentev Stanislav <hvoinii@gmail.com>
2021-06-26 15:26:37 +0600

    feat: styles and html for Header and Footer

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,21 +1,27 @@
 import React, { FC } from "react";
 import { Link } from "react-router-dom";
+import styled from "styled-components";
+import Logo from "../../assets/images/svg/header-logo.svg";
 
 const Header: FC<{}> = () => {
   return (
-    <header>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/login">Login</Link>
-          </li>
-          <li>
-            <Link to="/forgotpassword">password</Link>
-          </li>
-        </ul>
-      </nav>
-    </header>
+    <Root>
+      <Link to="/">
+        <img src={Logo} alt="Baseball cloud" />
+      </Link>
+    </Root>
   );
 };
+
+const Root = styled.header`
+  background: var(--white);
+  color: var(--gray2);
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  flex-wrap: wrap;
+  padding: 8px;
+  border-bottom: 1px solid rgba(0, 0, 0, 0.1);
+`;
 
 export default Header;
